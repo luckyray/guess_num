c89107ad10289a945be45d8cd31e4134e07f82e6
Raymond Liu <wfliu66@qq.com>
2020-09-18 09:53:21 +0100

    final version

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('please enter start of a random number range: ')
+end = input('please enter end of a random number range: ')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
 while True:
     count = count + 1
